aa567205f4fc079395f3b371d3ee7fe61d9393e3
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-12 13:55:36 -0300

    condicoes em javascript

---
+++ aula 11/exe008.js
@@ -0,0 +1,7 @@
+var vel = 62;
+console.log(`a velocidade do seu carro é de ${vel} km/h`);
+if(vel > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO');
+}
+    
+console.log('Dirija com cinto de segurança!')
