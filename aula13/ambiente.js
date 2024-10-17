0f9974e3e155e2eec823639833f732055a7a5716
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-17 10:16:39 -0300

    estrutura de repetição com "for"
    treinando estrutura de repetição com "for"

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,13 +1,21 @@
 var c = 1;
 while(c <= 2){
-    console.log(`Olá pela ${c}ª vez (usando While)`)
+    console.log(`Olá pela ${c}ª vez (usando While)`);
     //c += 1;
     c++
 }
+console.log('');
 
 var c2 = 1;
 do{
-    console.log(`Olá pela ${c2}ª vez (usando Do)`)
+    console.log(`Olá pela ${c2}ª vez (usando Do)`);
     //c += 1;
     c2++
 }while(c2 <= 2);
+console.log('');
+
+//var c3 = 1;
+for(var c3 = 1; c3 <= 2; c3++){
+    console.log(`Olá pela ${c3}ª vez (usando For)`);
+}
+console.log('');
