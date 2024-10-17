3bba8f9da3fdee474d89d3a02ee733324028fcee
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-17 10:08:47 -0300

    estrutura de repetição "Do"
    treinando estrutura de repetição "Do"

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,6 +1,13 @@
 var c = 1;
-while(c <= 6){
-    console.log(`Olá pela ${c}ª vez`)
+while(c <= 2){
+    console.log(`Olá pela ${c}ª vez (usando While)`)
     //c += 1;
     c++
 }
+
+var c2 = 1;
+do{
+    console.log(`Olá pela ${c2}ª vez (usando Do)`)
+    //c += 1;
+    c2++
+}while(c2 <= 2);
