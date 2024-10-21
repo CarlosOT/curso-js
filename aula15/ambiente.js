ed67a0fe47ae1f834d0a860c485a4e20f72a2aab
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-21 12:06:14 -0300

    variáveis compostas [arrays/vetor]
    utilizando "for" para mostrar a os elementos e suas respectivas posições de índice

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,19 +1,23 @@
 let num = [5,8,2,9,3];
-num[5] = 10;
-num[6] = 11;
-num[7] = 12;
+let c = 0;
 
 console.log(`Elementos do vetor antes do método sort(): ${num}`);
 
+//usando o método push o javascript adiciona o elemento no último índice
+num.push(1);
+console.log(`Elementos do vetor antes do método sort() com push(): ${num}`);
+
 //colocando todos os elementos em ordem
 num.sort()
-console.log(`Elementos do vetor depois do método sort(): ${num}`);
-
-//usando o método push o javascript adiciona o elemento no último índice
-num.push(500);
-console.log(`Elementos do vetor depois do método sort() com push(): ${num}`);
+console.log(`Elementos do vetor depois do método sort() e push(): ${num}`);
 
 //
 console.log('')
-console.log(`Elemento dentro do vetor no índice 7: ${num[7]}`)
+console.log(`Elemento dentro do vetor no índice 4: ${num[4]}`)
 console.log(`Tamanho do vetor: ${num.length}`);
+
+//mostrando elementos dentro do vetor usando for
+console.log('')
+for(c; c < (num.length); c++){
+    console.log(`Posição do índice [${c}]: ${num[c]}`);
+}
